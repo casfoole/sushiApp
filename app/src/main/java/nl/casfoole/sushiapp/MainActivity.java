8c22738abde95bc0f37d44fbdbf5fbd5943fdc4a
cas foole <casfoole2000@gmail.com>
2018-06-15 12:27:46 +0200

    Initial commit

---
+++ app/src/main/java/nl/casfoole/sushiapp/MainActivity.java
@@ -0,0 +1,102 @@
+package nl.casfoole.sushiapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    public void ToSushi1(View view)
+    {
+        Intent a = new Intent(MainActivity.this, Sushi1.class);
+        startActivity(a);
+    }
+    public void ToSushi2(View view){
+       Intent b = new Intent(MainActivity.this, Sushi2.class);
+        startActivity(b);
+    }
+    public void ToSushi3(View view){
+       Intent c = new Intent(MainActivity.this, Sushi3.class);
+        startActivity(c);
+    }
+    public void ToSushi4(View view){
+       Intent d = new Intent(MainActivity.this, Sushi4.class);
+        startActivity(d);
+    }
+     public void ToSushi5(View view){
+       Intent d = new Intent(MainActivity.this, Sushi5.class);
+        startActivity(d);
+    }
+     public void ToSushi6(View view){
+       Intent d = new Intent(MainActivity.this, Sushi6.class);
+        startActivity(d);
+    }
+     public void ToSushi7(View view){
+       Intent d = new Intent(MainActivity.this, Sushi7.class);
+        startActivity(d);
+    }
+     public void ToSushi8(View view){
+       Intent d = new Intent(MainActivity.this, Sushi8.class);
+        startActivity(d);
+    }
+     public void ToSushi9(View view){
+       Intent d = new Intent(MainActivity.this, Sushi9.class);
+        startActivity(d);
+    }
+     public void ToSushi10(View view){
+       Intent d = new Intent(MainActivity.this, Sushi10.class);
+        startActivity(d);
+    }
+     public void ToSushi11(View view){
+       Intent d = new Intent(MainActivity.this, Sushi11.class);
+        startActivity(d);
+    }
+     public void ToSushi12(View view){
+       Intent d = new Intent(MainActivity.this, Sushi12.class);
+        startActivity(d);
+    }
+     public void ToSushi13(View view){
+       Intent d = new Intent(MainActivity.this, Sushi13.class);
+        startActivity(d);
+    }
+     public void ToSushi14(View view){
+       Intent d = new Intent(MainActivity.this, Sushi14.class);
+        startActivity(d);
+    }
+     public void ToSushi15(View view){
+       Intent d = new Intent(MainActivity.this, Sushi15.class);
+        startActivity(d);
+    }
+     public void ToSushi16(View view){
+       Intent d = new Intent(MainActivity.this, Sushi16.class);
+        startActivity(d);
+    }
+     public void ToSushi17(View view){
+       Intent d = new Intent(MainActivity.this, Sushi17.class);
+        startActivity(d);
+    }
+     public void ToSushi18(View view){
+       Intent d = new Intent(MainActivity.this, Sushi18.class);
+        startActivity(d);
+    }
+     public void ToSushi19(View view){
+       Intent d = new Intent(MainActivity.this, Sushi19.class);
+        startActivity(d);
+    }
+     public void ToSushi20(View view){
+       Intent d = new Intent(MainActivity.this, Sushi20.class);
+        startActivity(d);
+    }
+
+}
